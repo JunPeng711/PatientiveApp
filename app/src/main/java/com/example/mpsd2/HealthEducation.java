12ea196807c113bed707c399306368a1820ed780
danyshrider <danyshrider@gmail.com>
2022-07-18 20:03:34 +0800

    MPSD2_Updated

--- app/src/main/java/com/example/mpsd2/HealthEducation.java
+++ app/src/main/java/com/example/mpsd2/HealthEducation.java
@@ -1,24 +1,14 @@
 package com.example.mpsd2;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HealthEducation extends AppCompatActivity implements View.OnClickListener{
+import android.os.Bundle;
 
+public class HealthEducation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.health_education);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
+        setContentView(R.layout.activity_health_education);
     }
 }
