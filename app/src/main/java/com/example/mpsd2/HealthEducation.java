a2d06a31631fe786542992c8c38c615a255efb4a
arfuu <ahfuu20@gmail.com>
2022-07-19 20:28:51 +0800

    update HealthEducation (JP)

--- app/src/main/java/com/example/mpsd2/HealthEducation.java
+++ app/src/main/java/com/example/mpsd2/HealthEducation.java
@@ -4,18 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class HealthEducation extends AppCompatActivity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_education);
 
+        initWidget();
+    }
 
+    private void initWidget()
+    {
         Button homeBtn, healthEducationBtn, btn1;
+        Button filterAllBtn, filterCovidBtn, filterExerciseBtn;
 
         homeBtn = findViewById(R.id.footBtn1);
         homeBtn.setOnClickListener(this);
@@ -25,7 +33,11 @@ public class HealthEducation extends AppCompatActivity implements View.OnClickLi
 
         btn1 = findViewById(R.id.showBtn1);
         btn1.setOnClickListener(this);
+
+
     }
+
+
 
 
     @Override
@@ -40,6 +52,7 @@ public class HealthEducation extends AppCompatActivity implements View.OnClickLi
             case R.id.showBtn1:
                 startActivity(new Intent(this, Modules1.class));
                 break;
+
         }
     }
 
