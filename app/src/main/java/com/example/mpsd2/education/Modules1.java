1590ee1440a5412352c190720758cf647e7c126a
arfuu <ahfuu20@gmail.com>
2022-08-03 22:20:20 +0800

    add video in Modules1 (JP)

--- app/src/main/java/com/example/mpsd2/education/Modules1.java
+++ app/src/main/java/com/example/mpsd2/education/Modules1.java
@@ -3,9 +3,12 @@ package com.example.mpsd2.education;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.example.mpsd2.R;
 import com.example.mpsd2.education.HealthEducation;
@@ -22,6 +25,15 @@ public class Modules1 extends AppCompatActivity implements View.OnClickListener 
         backToHomeBtn = findViewById(R.id.back);
         backToHomeBtn.setOnClickListener(this);
 
+        //wash hand video
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.wash_hand_video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 
 
