b0dc42398f984ee6ed1ae0193ef1b6e2d4ba71b5
BeepBoop <hsienlai2@gmail.com>
2022-08-06 02:58:50 +0800

    check in

--- app/src/main/java/com/example/mpsd2/CheckIn.java
+++ app/src/main/java/com/example/mpsd2/CheckIn.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,7 +24,7 @@ import java.util.Calendar;
 
 public class CheckIn extends AppCompatActivity implements View.OnClickListener {
 
-    Button backToHomeBtn;
+    ImageButton backToHomeBtn;
 
     private FirebaseUser user;
     private DatabaseReference reference;
